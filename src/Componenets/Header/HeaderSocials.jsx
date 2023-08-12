64985b250970f4d99ce4e40cda5ff7402b68bc9d
TyRoopam9599 <roopamtyagi72@gmail.com>
2023-08-12 09:59:24 +0530

    Edit links

--- src/Componenets/Header/HeaderSocials.jsx
+++ src/Componenets/Header/HeaderSocials.jsx
@@ -5,9 +5,8 @@ const HeaderSocials = () => {
     <div className='header__socials'>
         <a href='https://linkedin.com/in/roopamtyagi' target='blank'><SiLinkedin size={25}/></a>
         <a href='https://github.com/TyRoopam9599' target='blank'><SiGithub size={25}/></a>
-        <a href='https://leetcode.com/roopam_9599' target='blank'><SiLeetcode size={25}/></a>
+        <a href='https://leetcode.com/roopam9599' target='blank'><SiLeetcode size={25}/></a>
         <a href='https://hackerrank.com/roopamtyagi72' target='blank'><SiHackerrank size={25}/></a>
-        {/* <a href='https://geeksforgeeks.org/profile/roopamtyagi72' target='blank'><SiGeeksforgeeks/></a> */}
     </div>
   )
 }
