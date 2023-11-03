f671956df08656cb220ecd0e1eb9cfdeb46edce8
TyRoopam9599 <roopamtyagi72@gmail.com>
2023-11-03 12:01:04 +0530

    Medium Added

--- src/Componenets/Header/HeaderSocials.jsx
+++ src/Componenets/Header/HeaderSocials.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
-import {SiLinkedin, SiGithub, SiLeetcode, SiHackerrank} from 'react-icons/si'
+import {SiLinkedin, SiGithub, SiLeetcode, SiHackerrank, SiMedium} from 'react-icons/si'
 const HeaderSocials = () => {
   return (
     <div className='header__socials'>
         <a href='https://linkedin.com/in/roopamtyagi' target='blank'><SiLinkedin size={25}/></a>
+        <a href='https://medium.com/@roopamtyagi72' target='blank'><SiMedium size={25}/></a>
         <a href='https://github.com/TyRoopam9599' target='blank'><SiGithub size={25}/></a>
         <a href='https://leetcode.com/roopam9599' target='blank'><SiLeetcode size={25}/></a>
         <a href='https://hackerrank.com/roopamtyagi72' target='blank'><SiHackerrank size={25}/></a>
